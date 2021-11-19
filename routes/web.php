3260dec8a4bf99aec516c085808cb8d503aee324
Billy Hans Weno <billyhans90@gmail.com>
2021-11-19 13:38:00 +0000

    Add the the edit page and parse the corresponding data into the page

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,8 @@ Route::get('/brand/all', [BrandController::class, 'AllBrand'])->name('all.brand'
 
 Route::post('/brand/add',[BrandController::class, 'StoreBrand'])->name('store.brand');
 
+Route::get('/brand/edit/{id}', [BrandController::class, 'Edit']);
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     // $users = User::all();

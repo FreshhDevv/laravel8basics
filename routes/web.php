35c61405475618271618dcac4f16c7a8bd9cd20d
FreshDev <billyhans90@gmail.com>
2021-11-21 21:15:16 +0000

    Set up the admin page for the company site

--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,6 @@ Route::post('/multi/add', [BrandController::class, 'StoreImage'])->name('store.i
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     // $users = User::all();
-    $users = DB::table('users')->get();
-    return view('dashboard', compact('users'));
+    // $users = DB::table('users')->get();
+    return view('admin.index');
 })->name('dashboard');
